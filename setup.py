9d3a59036dc0d117034af4cc4c4ee2d280ae7752
Adam Presley <adam@adampresley.com>
2012-11-07 23:51:43 -0600

    Increment build to 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name = "placefinder-py",
-	version = "0.2",
+	version = "0.3",
 	description = "Yahoo! BOSS PlaceFinder Python Client",
 	long_description = "placefinder-py is a small Python module that provides the ability to work with Yahoo's BOSS PlaceFinder geocoding server (http://developer.yahoo.com/boss/geo/). This module provides all the basic functions needed to geocode address to latitude/longitude values as well as reverse-geocoding.",
 	author = "Adam Presley",
